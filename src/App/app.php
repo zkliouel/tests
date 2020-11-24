76c6e559dd640bd0c5f935b2f0d124c5aa422e66
zkliouel <zkliouel@gmail.com>
2020-11-24 10:55:15 +0100

    Update app.php

--- src/App/app.php
+++ src/App/app.php
@@ -1,40 +1,72 @@
 <?php
+
 namespace App;
 
-use vehicle;
-use parkVehicle;
-
-class app 
+/** @todo create specific exception */
+/** @todo manage exception */
+class App
 {
-  /** @var $myFleet vehicle[] */
-  protected $myFleet;
-  
-  /** @var $parkVehicleService parkVehicle */
-  protected $parkVehicleService;
-  
-  /**
-   * App Constructor
-   * 
-   *@param parkVehicle $parkVehicleService
-   */
-  public function __construct(parkVehicle $parkVehicleService)
-  {
-    $this->myFleet = [];
-    $this->parkVehicleService = $parkVehicleService;
-  }
-  
-  public function registerVehicle(array $data)
-  {
-    if(empty($data) || !isset($data['type']) || !isset($data['licence_plate']) || !isset($data['name']) {
-      throw new \Exception('Vehicle data not valid');
+    /** @var $myFleet Vehicle[] */
+    protected $myFleet;
+
+    /** @var $parkVehicleService ParkVehicle */
+    protected $parkVehicleService;
+
+    /**
+     * App Constructor
+     *
+     * @param ParkVehicle $parkVehicleService
+     */
+    public function __construct(ParkVehicle $parkVehicleService)
+    {
+        $this->myFleet = [];
+        $this->parkVehicleService = $parkVehicleService;
     }
-    if(isset($this->myFleet[$data['licence_plate']])) {
-      throw new \Exception('Vehicle already registred');
+
+    /**
+     * @param array $vehicleData
+     *
+     * @throws \Exception
+     *
+     * @todo move it into a service
+     * @todo use const for keys
+     */
+    public function registerVehicle(array $vehicleData)
+    {
+        if (!Vehicle::validateData($vehicleData)) {
+            throw new \Exception('Vehicle data not valid');
+        }
+        if (isset($this->myFleet[$vehicleData['licence_plate']])) {
+            throw new \Exception('Vehicle already registred');
+        }
+        $vehicle = new Vehicle();
+        $vehicle->populateFromArray($vehicleData);
+        $this->myFleet[$vehicleData['licence_plate']] = $vehicle;
     }
-    $vehicle = new vehicle();
-    $vehicle->populateFromArray($data); 
-    $this->myFleet[$data['licence_plate']] = $vehicle; 
-  }
-       
-   
+
+    /**
+     * @param string $licencePlate
+     * @param array  $geoLoc
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function parkVehicle(string $licencePlate, array $geoLoc)
+    {
+        if (!isset($this->myFleet[$licencePlate])) {
+            throw new \Exception('Vehicle not in fleet, register it');
+        }
+        $vehicle = $this->myFleet[$licencePlate];
+        if(empty($vehicle->getGeoLoc())) {
+            $vehicle->setGeoLoc($geoLoc);
+            return true;
+        }
+        try {
+            $this->parkVehicleService->parkVehicle($vehicle, $geoLoc);
+            return true;
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
 }
