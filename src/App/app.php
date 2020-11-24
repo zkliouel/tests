456a15bb931380f03897ef2597c816ea38c6c1ca
zkliouel <zkliouel@gmail.com>
2020-11-24 08:34:05 +0100

    Update app.php

--- src/App/app.php
+++ src/App/app.php
@@ -11,4 +11,30 @@ class app
   
   /** @var $parkVehicleService parkVehicle */
   protected $parkVehicleService;
+  
+  /**
+   * App Constructor
+   * 
+   *@param parkVehicle $parkVehicleService
+   */
+  public function __construct(parkVehicle $parkVehicleService)
+  {
+    $this->myFleet = [];
+    $this->parkVehicleService = $parkVehicleService;
+  }
+  
+  public function registerVehicle(array $data)
+  {
+    if(empty($data) || !isset($data['type']) || !isset($data['licence_plate']) || !isset($data['name']) {
+      throw new \Exception('Vehicle data not valid');
+    }
+    if(isset($this->myFleet[$data['licence_plate']])) {
+      throw new \Exception('Vehicle already registred');
+    }
+    $vehicle = new vehicle();
+    $vehicle->populateFromArray($data); 
+    $this->myFleet[$data['licence_plate']] = $vehicle; 
+  }
+       
+   
 }
