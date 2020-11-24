f58949e56e7792fe2468b65979e1a61f4206d093
zkliouel <zkliouel@gmail.com>
2020-11-24 10:55:51 +0100

    Update parkVehicle.php

--- src/App/parkVehicle.php
+++ src/App/parkVehicle.php
@@ -1,8 +1,21 @@
 <?php 
-namespace App/
-
-use vehicle;
+namespace App;
 
 class parkVehicle
 {
+
+    /**
+     * @param \App\Vehicle $vehicle
+     * @param array        $geoLoc
+     *
+     * @return bool
+     */
+    public function parkVehicle(Vehicle $vehicle, array $geoLoc):bool
+    {
+        $vehicleGeoloc = $vehicle->getGeoLoc();
+        if(empty(array_diff($vehicleGeoloc, $geoLoc))) {
+            throw new \Exception('Vehicule already  parked a this location');
+        }
+        $vehicle->setGeoLoc($geoLoc);
+    }
 }
